a87d3d240c7771261caf0fc0fa1ba44174453086
zhaoliang <zll_space@163.com>
2018-04-22 14:06:17 +0800

    spring boot 学习

---
+++ boots-line-road/src/main/java/com/line/road/BootsLineRoadApplication.java
@@ -0,0 +1,12 @@
+package com.line.road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsLineRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsLineRoadApplication.class, args);
+	}
+}
